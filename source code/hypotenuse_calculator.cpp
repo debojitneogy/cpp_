35466bc4eb44c9d4d0ee55d22fefffed82ea013c
debojitneogy <157280092+debojitneogy@users.noreply.github.com>
2024-06-20 01:23:30 +0530

    added hypotenuse_calculator

---
+++ source code/hypotenuse_calculator.cpp
@@ -0,0 +1,21 @@
+#include <cmath>
+#include <iostream>
+using std::cout;
+using std::cin;
+
+int main(){
+    double n1;
+    double n2;
+    double result;
+
+    cout << "enter base: ";
+    cin >> n1;
+
+    cout << "enter height: ";
+    cin >> n2;
+
+    result = sqrt(pow(n1,2) + pow(n2,2));
+    cout << "the result is: " << result;
+
+    return 0;
+}
